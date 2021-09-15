2554e27d3e59a6f23b8fbb3cdce7aed2f6b83dfe
Lingga Pratama <linggapratama67@gmail.com>
2021-09-15 16:51:22 +0700

    Set crossDomain true

--- src/api/home.js
+++ src/api/home.js
@@ -2,13 +2,6 @@ import axios from 'axios';
 
 import { config } from '../config';
 
-export async function getHomes(params){
-    let formBody = [];
-    for (var property in params) {
-        var encodedKey      = encodeURIComponent(property);
-        var encodedValue    = encodeURIComponent(params[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-    }
-    formBody = formBody.join("&");
-    return await axios.get( `${config.api_host}indonesia`, {headers:{'Access-Control-Allow-Origin': '*'}});
+export async function getHomes(){
+    return await axios.get( `${config.api_host}indonesia`, { crossDomain: true });
 }
--- src/api/province.js
+++ src/api/province.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 import { config } from '../config';
 
-export async function getProvince(params){
-    return await axios.get( `${config.api_host}indonesia/provinsi`, {headers:{'Access-Control-Allow-Origin': '*'}});
+export async function getProvince(){
+    return await axios.get( `${config.api_host}indonesia/provinsi`, { crossDomain: true });
 }
