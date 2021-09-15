35d25e8b269794ecf1e6d622757d7b8fabdeabb4
Lingga Pratama <linggapratama67@gmail.com>
2021-09-15 16:35:32 +0700

    Add header

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -10,7 +10,7 @@ export default function TopBar (){
                 <div className="top-menu-area">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-6 col-md-66 col-6 v_middle pt-3 pb-3">
+                            <div className="col-lg-6 col-md-6 col-6 v_middle pt-3 pb-3">
                                 <Link to="/">
                                 <div> <h5>Data Kasus Coronavirus di Indonesia</h5> </div>
                                 </Link>
